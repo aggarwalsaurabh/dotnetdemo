2dc92548d42579c95f84efb4c1b9fdf8dbfc8c0a
Saurabh aggarwal <saurabhfamilymail@gmail.com>
2023-09-27 11:27:56 +0530

    adding try and catch

--- Program.cs
+++ Program.cs
@@ -3,4 +3,19 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World! V2");
 
+app.MapGet("/creditscore", () =>
+{
+    try
+    {
+        var result = 3/0;
+        Console.WriteLine($"Processing succeeded: {result}");
+    }
+    catch (Exception e)
+    {
+        LogError(e, "Processing failed.");
+        throw;
+    }
+    return score;
+});
+
 app.Run();
