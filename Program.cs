db22457abfe84250a6b1a40f94b558c053e73386
Saurabh aggarwal <saurabhfamilymail@gmail.com>
2023-09-27 11:46:36 +0530

    Added try catch

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
+using System;
+using System.IO;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World! V3");
 
+app.MapGet("/score", () =>
+{
+        try
+        {
+            using (StreamReader sr = File.OpenText("data.txt"))
+            {
+                Console.WriteLine($"The first line of this file is {sr.ReadLine()}");
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine($"The file was not found: '{e}'");
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            Console.WriteLine($"The directory was not found: '{e}'");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"The file could not be opened: '{e}'");
+        }
+});
 
 app.Run();
