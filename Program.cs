9222796c22d40fe032575d88b6b68bd84034c5de
Saurabh aggarwal <saurabhfamilymail@gmail.com>
2023-09-27 11:37:38 +0530

    Correcting program

--- Program.cs
+++ Program.cs
@@ -1,20 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! V2");
+app.MapGet("/", () => "Hello World! V3");
 
-app.MapGet("/creditscore", () =>
-{
-    try
-    {
-        Console.WriteLine($"Processing succeeded: {3 + 5}");
-    }
-    catch (Exception e)
-    {
-        LogError(e, "Processing failed.");
-        throw;
-    }
-    return score;
-});
 
 app.Run();
